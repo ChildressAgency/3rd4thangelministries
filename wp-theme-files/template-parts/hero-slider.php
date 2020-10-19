ca0c50cec5f5a2264b469b2dd076b4441a683024
Mikhail Novikov <ishy@likes.vodka>
2020-10-18 21:28:29 -0400

    hero slider

---
+++ wp-theme-files/template-parts/hero-slider.php
@@ -0,0 +1,30 @@
+<?php
+$images = get_field("hero_images");
+if ($images):
+    ?>
+
+    <div class="container-fluid hero-slider p-0">
+        <div class="row">
+            <div class="col">
+
+                <div id="heroSlider" class="carousel slide" data-ride="carousel" data-interval="2000">
+                    <ol class="carousel-indicators">
+                        <?php foreach ($images as $index => $image) : ?>
+                            <li data-target="#heroSlider" data-slide-to="<?php echo $index ?>"
+                                class="<?php echo $index == 0 ? "active" : "" ?>"></li>
+                        <?php endforeach; ?>
+                    </ol>
+                    <div class="carousel-inner">
+                        <?php foreach ($images as $index => $image) : ?>
+                            <div class="carousel-item <?php echo $index == 0 ? "active" : "" ?>">
+                                <img class="d-block w-100" src="<?php echo esc_url($image) ?>" alt="">
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<?php endif;
