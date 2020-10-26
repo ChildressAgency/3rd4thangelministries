34aba162099041e603342663c6972c206ccb775c
Mikhail Novikov <ishy@likes.vodka>
2020-10-26 19:44:27 -0400

    footer with copyright/privacy policy

--- wp-theme-files/footer.php
+++ wp-theme-files/footer.php
@@ -1,4 +1,14 @@
 <?php wp_footer(); ?>
 
+<footer class="text-center py-3 mt-5">
+    <small>&copy; <?php echo date("Y") ?> 3rd &amp; 4th Angels Ministries
+        <span class="d-none d-md-inline">&bullet;</span>
+        <span class="d-block d-md-inline">
+            All rights reserved &bullet;
+            <a href="<?php echo get_privacy_policy_url() ?>">Privacy Policy</a>
+        </span>
+    </small>
+</footer>
+
 </body>
 </html>
