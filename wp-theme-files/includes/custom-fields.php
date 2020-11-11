fc0ab48cb221ac93fdcaae1effc1bac4fbd17f54
Mikhail Novikov <ishy@likes.vodka>
2020-11-10 23:10:15 -0500

    update custom field definitions

--- wp-theme-files/includes/custom-fields.php
+++ wp-theme-files/includes/custom-fields.php
@@ -518,6 +518,46 @@ if (function_exists('acf_add_local_field_group')):
     ));
 
     acf_add_local_field_group(array(
+        'key' => 'group_5fab5fbf9b2d1',
+        'title' => 'YouTube Video',
+        'fields' => array(
+            array(
+                'key' => 'field_5fab60073ab7b',
+                'label' => 'YouTube Video URL',
+                'name' => 'video',
+                'type' => 'url',
+                'instructions' => 'Enter the URL of the video, for example https://www.youtube.com/watch?v=wT-ahL4CIP4',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'video',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
         'key' => 'group_5f924369e9cf8',
         'title' => 'Donation Message',
         'fields' => array(
