0cb8e7cedc745bf918b2a418c21b9ecdb807f683
Mikhail Novikov <ishy@likes.vodka>
2020-11-12 23:49:51 -0500

    use a thumbnail version with no letterbox

--- wp-theme-files/template-parts/video.php
+++ wp-theme-files/template-parts/video.php
@@ -2,10 +2,10 @@
 $url = get_field("video");
 preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $url, $match);
 $video_id = $match[1];
-$thumb_url = "https://i.ytimg.com/vi/$video_id/hqdefault.jpg";
+$thumb_url = "https://i.ytimg.com/vi/$video_id/maxresdefault.jpg";
 ?>
 
-<a class="col-12 col-md-6 col-lg-4 text-center p-3 p-lg-5 stripe-item loadmore-item"
+<a class="col-12 col-md-6 col-lg-4 text-center p-1 p-lg-3 stripe-item loadmore-item"
    target="_blank" href="<?php echo $url; ?>">
     <img class="img-fluid" src="<?php echo $thumb_url; ?>"
          alt="<?php echo the_title() ?>" title="<?php echo the_title() ?>"/>
